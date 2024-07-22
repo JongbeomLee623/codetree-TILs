bd309f266aa9bacbe1f6be9226672f3a7c79fc00
Jongbeom Lee <116253832+JongbeomLee623@users.noreply.github.com>
2024-07-22 15:47:22 +0900

    Title: 다양하게 출력, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/다양하게 출력/print-in-variety.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append("Total days in Year"+"\n"+
+"365"+"\n"+
+"Circumference rate"+"\n"+
+"3.1415926535");
+        System.out.print(sb);
+    }
+}
