ff016cb23bcaa3280471eb43a62ccab4bb9a146b
Jongbeom Lee <116253832+JongbeomLee623@users.noreply.github.com>
2024-07-22 17:34:27 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 225ms, Memory: 25MB, Status: Passed - CodeTree

--- 240722/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
+++ 240722/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -4,24 +4,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 public class Main {
     static StringBuilder sb = new StringBuilder();
+    static int K, N;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        pm(K,N);
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        
+        arr = new int[N];
+        dfs(0);
+
         System.out.print(sb);
-
-
     }
-    public static void pm (int x, int y){
-        for(int i = 1; i<=x; i++){
-            for(int j = 1; j<=y; j++){
-                sb.append(i + " "+ j);
-                sb.append("\n");
+    public static void dfs (int k){
+        if(k==N){
+            for(int value: arr){
+                sb.append(value+" ");
             }
+            sb.append("\n");
+            return;    
         }
+        for(int i=1; i<=K; i++){
+            arr[k]=i;
+            dfs(k+1);
+        }
+        
     }
 }
