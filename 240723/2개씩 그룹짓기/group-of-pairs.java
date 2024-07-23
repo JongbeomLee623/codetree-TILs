5d2a16f40d30e43cca6fadfb89cbbc875bd29414
Jongbeom Lee <116253832+JongbeomLee623@users.noreply.github.com>
2024-07-23 14:39:47 +0900

    Title: 2개씩 그룹짓기, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240723/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static ArrayList<Integer> arr =  new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr.add(Integer.parseInt(st.nextToken()));
+        }   
+
+        int max = Integer.MIN_VALUE;
+        Collections.sort(arr);
+        for(int i=0; i<N; i++){
+            int r = arr.get(i)+arr.get(N-1-i);
+
+            if(r > max){
+                max = r;
+            }
+        }
+
+        System.out.print(max);
+    }
+}
