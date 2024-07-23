26c21433baa0ad5ccce0bcba8c7ac871c80e2096
Jongbeom Lee <116253832+JongbeomLee623@users.noreply.github.com>
2024-07-23 11:41:11 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 79ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240723/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    public static int[] arr;
+    public static int[] ans;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        ans = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i =0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            ans[i] = 1;
+        }
+
+        for(int i=1; i<=n-1; i++){
+            ans[i]=lcm(arr[i-1],ans[i-1]);
+        }
+
+        System.out.print(ans[n-1]);
+    }
+    public static int gcd(int a, int b){
+        int r = a % b;
+        while(b!=0){
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        
+        return a;
+    }
+    
+    public static int lcm(int a, int b){
+        return (a*b)/gcd(a,b);
+    }
+
+
+}
