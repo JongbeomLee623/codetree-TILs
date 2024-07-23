0619308a95a0d673e88488dad64d95c2085b320b
Jongbeom Lee <116253832+JongbeomLee623@users.noreply.github.com>
2024-07-23 15:10:55 +0900

    Title: k번째로 신기한 문자열, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240723/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        String T = st.nextToken();
+
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i = 0; i<n; i++){
+            String str = br.readLine();
+            if(str.contains(T)){
+                arr.add(str);
+            }
+        }
+        Collections.sort(arr);
+        System.out.print(arr.get(k-1));
+        
+    }
+}
